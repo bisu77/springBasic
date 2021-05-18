026a2adbd3385f1a371e3effdd2cdedd14fbcc56
YKJ <rudwns7552@gmail.com>
2021-05-18 11:03:42 +0900

    # Spring Basic Study   1) @ComponetScan이 붙은 클래스의 패키지의 하위 패키지를 기준으로 @Componet 어노테이션을      읽어들여서 스프링 컨테이너에 등록      - @Autowired를 통한 의존자동주입 가능      - AppConfig 클래스의 @Configuration > @Bean과 동일한 싱글톤 컨테이너      - @Controller, @Repository, @Service, @Configuration 모두 @Componet 속성을        갖고 있기 때문에 @ComponetScan 대상      - @ComponetScan을 갖는 어노테이션은 루트 프로젝트에 위치하기.        => 제외하고 싶은 패키지 or 클래스는 excludeFilters 속성 사용

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
+)//컴포넌트 스캔 시 @Configuration 어노테이션 붙은 class 제외하도록 처리(기존 코드 AppConfig.java 실행하지 않음)
+public class AutoAppConfig {
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,13 +4,17 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     //private DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
