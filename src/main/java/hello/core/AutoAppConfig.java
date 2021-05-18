25405430cceedf634cfaafe4507908bb5d610836
YKJ <rudwns7552@gmail.com>
2021-05-18 11:13:15 +0900

    # Spring Basic Study   1) 중복등록 : @ComponetScan을 통한 자동 Bean 등록 vs @Bean을 통한 수동 Bean 등록      - 수동 Bean 등록이 우선시 되어 Override 됨      - 하지만, 설정이 꼬일 수 있기 때문에, 최근 스프링부트는 기동 시 오류 발생        => Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true        => 로그에 보이는 allow-bean-definition-overriding=true 옵션으로 수동 오버라이딩 처리 가능

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,4 +11,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )//컴포넌트 스캔 시 @Configuration 어노테이션 붙은 class 제외하도록 처리(기존 코드 AppConfig.java 실행하지 않음)
 public class AutoAppConfig {
+    @Bean(name="memoryMemberRepository")
+    MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
