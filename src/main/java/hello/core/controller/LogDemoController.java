b82013246c59e5ad341fbd8077f8b14a956b09d0
YKJ <rudwns7552@gmail.com>
2021-06-04 14:46:29 +0900

    # Spring Basic Study   3) request 스코프 예제      - ObjectProvider를 이용하여 DI 지연주입      - getObject() 메서드 호출 시점에 빈 생성(동일 request > 싱글톤 방식)

--- src/main/java/hello/core/controller/LogDemoController.java
+++ src/main/java/hello/core/controller/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.controller;
 import hello.core.common.MyLogger;
 import hello.core.logdemo.LogDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-    private final MyLogger logger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     private final LogDemoService logDemoService;
 
     @RequestMapping("/logger")
     @ResponseBody
-    public String logger(HttpServletRequest request){
-        logger.setRequestURL(request.getRequestURL().toString());
+    public String logger(HttpServletRequest request) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(request.getRequestURL().toString());
 
-        logger.log("controller message");
+        myLogger.log("controller message");
+        Thread.sleep(1000);
         logDemoService.logic("service message");
+
         return "OK";
     }
 }
